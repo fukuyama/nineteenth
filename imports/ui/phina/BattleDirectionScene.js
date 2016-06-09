e0952e86c30ef68bfeadb28dd101efac1d84d5b3
Yoshihito Fukuyama <yoshihito.fukuyama@gmail.com>
2016-06-09 23:48:47 +0900

    make...

--- imports/ui/phina/BattleDirectionScene.js
+++ imports/ui/phina/BattleDirectionScene.js
@@ -22,5 +22,29 @@ phina.define('nz.BattleDirectionScene', {
   _movePointer(pointer) {
     console.log('_movePointer',pointer.position);
   }
+  /*
+  _rotatePointer(rotation) {
+    return unless @pointer?
+    rotation = nz.utils.normalizRotation rotation
+    for d,i in DIRECTIONS when 0 <= i and i < 6
+      if d.rotation - 30 <= rotation and rotation <= d.rotation + 30
+        costd = nz.Graph.directionCost(@target.direction, d.index)
+        if costd <= @_remnant
+          if @_direction != d.index
+            @_direction = d.index
+            @pointer.rotation = d.rotation
+            @_keyRotate = d.rotation
+            return
+    @_keyRotate = DIRECTIONS[@_direction].rotation
+  }
 
+  _movePointer: (pointing) ->
+    return unless @pointer?
+    t = @target.body.localToGlobal tm.geom.Vector2(0,0)
+    x = pointing.x - t.x
+    y = pointing.y - t.y
+    v = tm.geom.Vector2 x,y
+    @_rotatePointer Math.radToDeg v.toAngle()
+    return
+  */
 });
