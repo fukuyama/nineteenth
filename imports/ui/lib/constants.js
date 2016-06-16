1c348d8e4e953883c8a9f9e209652bcbff85929e
Yoshihito Fukuyama <yoshihito.fukuyama@gmail.com>
2016-06-16 21:35:50 +0900

    change direction default

--- imports/ui/lib/constants.js
+++ imports/ui/lib/constants.js
@@ -7,6 +7,6 @@ export const DIRECTIONS    = [
   {name:'down_right', rotation:   30, index:2, rotateIndex:[-2,-1, 0, 1, 2, 3]},
   {name:'down'      , rotation:   90, index:3, rotateIndex:[ 3,-2,-1, 0, 1, 2]},
   {name:'down_left' , rotation:  150, index:4, rotateIndex:[ 2, 3,-2,-1, 0, 1]},
-  {name:'up_left'   , rotation: -150, index:5, rotateIndex:[ 1, 2, 3,-2,-1, 0]},
-  {name:'default'   , rotation:   90, index:6, rotateIndex:[ 0, 1, 2, 3,-2,-1]}
+  {name:'up_left'   , rotation: -150, index:5, rotateIndex:[ 1, 2, 3,-2,-1, 0]}
+  //{name:'default'   , rotation:   90, index:6, rotateIndex:[ 0, 1, 2, 3,-2,-1]}
 ];
